002e3dbb38ecfde8c6a68bae2231d57f6cdf6ef8
Andres Kovalev <mail@akovalev.ru>
2020-08-14 23:02:15 +0300

    feat: implement Budgets component

---
+++ src/components/Budgets/Budget.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+import styles from './Budget.module.scss';
+
+export function Budget({ budget }) {
+    const [ isExpanded, setIsExpanded ] = useState(false);
+    const toggle = () => setIsExpanded((state) => !state);
+
+    const { name, indicators } = budget;
+    const { rest } = indicators.adaptive;
+
+    const className = cx(styles.budget, {
+        [styles.negative]: rest < 0
+    });
+
+    return (
+        <article className={ className } onClick={ toggle }>
+            <h2 className={ styles.title }>
+                { name }
+            </h2>
+            {renderBudgetContent(indicators, isExpanded)}
+        </article>
+    );
+}
+
+const indicatorProp = PropTypes.shape({
+    rest: PropTypes.number.isRequired
+});
+
+export const BudgetShape = PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    indicators: PropTypes.shape({
+        adaptive: indicatorProp
+    }).isRequired
+});
+
+Budget.propTypes = {
+    budget: BudgetShape.isRequired
+};
+
+function renderBudgetContent(indicators, isExpanded) {
+    const { average, adaptive, optimistic, full, prediction } = indicators;
+
+    if (!isExpanded) {
+        return (
+            <div className={ styles.rest }>
+                { adaptive.rest }
+            </div>
+        );
+    }
+
+    const [ min, avg, max ] = [
+        average.rest,
+        adaptive.rest,
+        optimistic.rest
+    ].sort(asc);
+
+    return (
+        <React.Fragment>
+            <div className={ styles.details }>
+                <div className={ styles.expense }>
+                    { adaptive.spent ? '-' : '' }
+                    { adaptive.spent }
+                </div>
+                <div className={ styles.rests }>
+                    { min }
+                    { ' ← ' }
+                    { avg }
+                    { ' → ' }
+                    { max }
+                </div>
+                <div className={ styles.fullRest }>
+                    { full.rest }
+                </div>
+            </div>
+            <div className={ styles.prediction }>
+                { prediction.rest }
+            </div>
+        </React.Fragment>
+    );
+}
+
+function asc(a, b) {
+    return a - b;
+}
---
+++ src/components/Budgets/Budgets.js
@@ -0,0 +1,73 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { createSelector } from '@reduxjs/toolkit';
+
+import { ReloadableBox } from '../ReloadableBox';
+import { tokenSelector } from '../../store/login';
+import { loadBudgets, budgetsSelector } from '../../store/budgets';
+import { hiddenSelector } from '../../store/settings';
+
+import { Budget } from './Budget';
+import { BudgetSkeleton } from './BudgetSkeleton';
+
+const selector = createSelector(
+    tokenSelector,
+    budgetsSelector,
+    hiddenSelector,
+    (token, budgets, hidden) => ({ token, budgets, hidden })
+);
+
+/* eslint-disable react/no-array-index-key */
+const loadingContent = (
+    <React.Fragment>
+        {Array(7).fill(0).map((_, key) => (
+            <BudgetSkeleton key={ `${ key }` } />
+        ))}
+    </React.Fragment>
+);
+/* eslint-enable react/no-array-index-key */
+
+export const Budgets = () => {
+    const [ isLoading, setIsLoading ] = useState(false);
+    const { token, budgets, hidden } = useSelector(selector);
+    const dispatch = useDispatch();
+    const [ , setError ] = useState('');
+
+    const reload = useCallback(() => {
+        setIsLoading(true);
+
+        dispatch(loadBudgets(token, (result) => {
+            if (result.error) {
+                setError(result.error);
+            }
+
+            setIsLoading(false);
+        }));
+    }, [ dispatch, token ]);
+
+    useEffect(() => {
+        if (budgets) {
+            return;
+        }
+
+        reload();
+    }, [ budgets, reload ]);
+
+    const firstLoad = isLoading && !budgets;
+    const itemsToLoad = (isLoading ? [] : (budgets || []))
+        .filter(({ id }) => !hidden.includes(id))
+        .sort(byPriority);
+
+    return (
+        <ReloadableBox loading={ isLoading && !firstLoad } reload={ reload } >
+            { isLoading && loadingContent }
+            {itemsToLoad.map(({ id, ...budget }) => (
+                <Budget key={ id } budget={ budget } />
+            ))}
+        </ReloadableBox>
+    );
+};
+
+function byPriority(budget1, budget2) {
+    return budget2.isPrimary - budget1.isPrimary;
+}
